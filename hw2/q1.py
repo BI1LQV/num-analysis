0038cff8de83723c24152fd1f221305706c09ade
BI1LQV <bi1lqv.y@gmail.com>
2022-09-26 14:31:19 +0800

    feat: 优化无唯一解情况

--- hw2/q1.py
+++ hw2/q1.py
@@ -1,13 +1,25 @@
-A = [
+from copy import deepcopy
+from functools import reduce
+
+A1 = [
     [10, -7, 0],
     [-3, 2, 6],
     [5, -1, 5]
 ]
 
+A2 = [
+    [10, -7, 0],
+    [10, -7, 0],
+    [5, -1, 5]
+]
+
+
 b = [7, 4, 6]
 
 
-def GaussSolve(A, b):
+def GaussSolve(iA, ib):
+    A = deepcopy(iA)
+    b = deepcopy(ib)
     n = len(A)
     x = [0]*n
     for k in range(0, n):
@@ -22,8 +34,21 @@ def GaussSolve(A, b):
             b[i] = b[i]-c*b[k]
 
     for i in range(0, n):
+        if A[i][i] == 0:
+            return None
         x[i] = b[i]/A[i][i]
     return x
 
 
-print(GaussSolve(A, b))
+def assess(A, b):
+    res = GaussSolve(A, b)
+    if res:
+        return reduce(
+            lambda before, cur: f"{before}x{cur[0]}={cur[1]}; ",
+            zip(range(1, len(res)+1), res), ""
+        )
+    return "无唯一解"
+
+
+print("对于A1:", assess(A1, b))
+print("对于A2:", assess(A2, b))
