4ea1b58c9980dfe7639cadc32911a9670a44efb5
BI1LQV <bi1lqv.y@gmail.com>
2022-10-19 14:25:36 +0800

    3.1基本完成

--- hw3/q1.py
+++ hw3/q1.py
@@ -6,9 +6,9 @@ from numpy import array, ndarray, zeros
 
 def calc(x, A, b, withCopy):
     toX = deepcopy(x)  # 故意深拷贝的 让函数无副作用
-    fromX = x
+    fromX = toX
     if withCopy:
-        fromX = deepcopy(x)
+        fromX = deepcopy(toX)
     for i in range(0, len(x)):
         sum1 = reduce(
             lambda sum, j: sum + A[i][j]*fromX[j],
@@ -17,21 +17,37 @@ def calc(x, A, b, withCopy):
     return toX
 
 
-def JacobSolve(A, b, e, x=None, delta=inf, iterTimes=0):
+def solver(A, b, e, isJacobi, x=None, delta=inf, iterTimes=0):
     if not (type(x) is ndarray):
         x = zeros(len(A))
     if delta < e:
         return x, iterTimes
-    nextX = calc(x, A, b, True)
+    nextX = calc(x, A, b, isJacobi)
     delta = reduce(
         lambda sum, i: sum + (nextX[i]-x[i])**2,
         range(0, len(A)), 0)
-    return JacobSolve(A, b, e, nextX, delta, iterTimes+1)
+    return solver(A, b, e, isJacobi, nextX, delta, iterTimes+1)
 
 
-res = JacobSolve(array([
+def JacobSolve(A, b, e):
+    return solver(A, b, e, True)
+
+
+def GSSolve(A, b, e):
+    return solver(A, b, e, False)
+
+
+jb = JacobSolve(array([
     [10, 3, 1],
     [2, -10, 3],
     [1, 3, 10]
-], dtype=float), array([14, -5, 14], dtype=float), 0.01)
-print(res)
+], dtype=float), array([14, -5, 14], dtype=float), 0.001)
+print(jb)
+
+
+gs = GSSolve(array([
+    [10, 3, 1],
+    [2, -10, 3],
+    [1, 3, 10]
+], dtype=float), array([14, -5, 14], dtype=float), 0.001)
+print(gs)
