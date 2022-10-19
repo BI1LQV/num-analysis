aa8c9f448a224bf81b98978688c6948c23329eee
BI1LQV <bi1lqv.y@gmail.com>
2022-10-19 21:49:30 +0800

    加一些注释

--- hw3/q1.py
+++ hw3/q1.py
@@ -6,14 +6,22 @@ from numpy.linalg import eigvals
 
 
 def isIterable(x):
-    return max(abs(eigvals(x))) < 1
+    base = zeros((len(x), len(x)))
+    LU = zeros((len(x), len(x)))
+    for i in range(len(x)):
+        for j in range(len(x)):
+            if i == j:
+                base[i][j] = 1/x[i][j]
+            else:
+                LU[i][j] = -x[i][j]
+    return max(abs(eigvals(base@LU))) < 1
 
 
 def calc(x, A, b, withCopy):
     toX = deepcopy(x)  # 故意深拷贝的 让函数无副作用
     fromX = toX
     if withCopy:
-        fromX = deepcopy(toX)
+        fromX = deepcopy(toX)  # 分别是Jacobi迭代法和Gauss-Seidel迭代法
     for i in range(0, len(x)):
         sum1 = reduce(
             lambda sum, j: sum + A[i][j]*fromX[j],
@@ -25,7 +33,7 @@ def calc(x, A, b, withCopy):
 def solver(A, b, e, isJacobi, x=None, delta=inf, iterTimes=0):
     if not (type(x) is ndarray):
         x = zeros(len(A))  # 初始化x
-    if not isIterable(x):  # 不收敛
+    if not isIterable(A):  # 不收敛
         return False
     if delta < e:
         return x, iterTimes  # 判停
@@ -59,3 +67,26 @@ gs = GSSolve(array([
     [1, 3, 10]
 ], dtype=float), array([14, -5, 14], dtype=float), 0.001)
 print(gs)
+
+jb = JacobSolve(array([
+    [1, 0, 0],
+    [0, 1, 1],
+    [0, -1, 1]
+], dtype=float), array([14, -5, 14], dtype=float), 0.001)
+print("Jacob", jb)
+
+
+gs = GSSolve(array([
+    [1, 0, 0],
+    [0, 1, 1],
+    [0, -1, 1]
+], dtype=float), array([14, -5, 14], dtype=float), 0.001)
+print("GS", gs)
+
+
+t2 = array([[3, 3], [2, -12]], dtype=float)
+
+print("JB", JacobSolve(t2, array([14, -5], dtype=float), 0.00001))
+
+
+print("GS", GSSolve(t2, array([14, -5], dtype=float), 0.00001))
