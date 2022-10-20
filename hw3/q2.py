d794187f9568b44c23c15b2a998cdd1f0d5030af
BI1LQV <bi1lqv.y@gmail.com>
2022-10-20 15:37:26 +0800

    fix 一些数值分析bug

--- hw3/q2.py
+++ hw3/q2.py
@@ -1,6 +1,7 @@
 from functools import reduce
-from sympy import symbols, integrate, poly
-
+from sympy import symbols, integrate, poly, simplify
+from numpy import arange
+from matplotlib.pyplot import plot, show, legend
 t = symbols('t')
 LEGEND_MAP = {
     0: 1+0*t,
@@ -28,8 +29,31 @@ def fitting(f, a, b, n):
             led*led, (t, a, b)
         ))*led
 
-    units = list(map(Unit, range(0, n)))
-    return poly(reduce(lambda sum, i: sum+i, units)).coeffs()
+    units = list(map(Unit, range(0, n+1)))
+    return poly(simplify(reduce(lambda sum, i: sum+i, units))).coeffs()
 
 
-print(fitting((1+t**2)**0.5, 0, 1, 2))
+coff = fitting((1+t**2)**0.5, 0, 1, 1)
+
+print(coff)
+
+
+xs = arange(0, 1, 0.001)
+
+
+def f1(x): return (1+x**2)**0.5
+
+
+def f2(x):
+    ret = 0
+    for i in range(0, len(coff)):
+        ret += coff[i]*x**(len(coff)-i-1)
+    return ret
+
+
+y1s = list(map(f1, xs))
+y2s = list(map(f2, xs))
+
+plot(xs, y1s)
+plot(xs, y2s)
+show()
